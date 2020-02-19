8b3fde8ada57cf9f501bff79abd4b0cfdcb57cc6
Bastien Rivière <babathriviere@gmail.com>
2020-02-19 18:45:35 +0100

    clean first and last indices

--- lib/daily_interview/fal_indices.ex
+++ lib/daily_interview/fal_indices.ex
@@ -9,16 +9,19 @@ defmodule DailyInterview.FalIndices do
   def get_indices(arr, target), do: get_indices(arr, target, 0, {-1, -1})
 
   # Match first index.
-  defp get_indices([head | tail], target, idx, {-1, -1}) when head == target,
-    do: get_indices(tail, target, idx + 1, {idx, -1})
+  defp get_indices([target | tail], target, idx, {-1, -1}) do
+    get_indices(tail, target, idx + 1, {idx, -1})
+  end
 
   # Match last index.
-  defp get_indices([head | tail], target, idx, {first, _last}) when head == target and first >= 0,
-    do: get_indices(tail, target, idx + 1, {first, idx})
+  defp get_indices([target | tail], target, idx, {first, _last}) when first >= 0 do
+    get_indices(tail, target, idx + 1, {first, idx})
+  end
 
   # Ignore values before first index.
-  defp get_indices([_head | tail], target, idx, {-1, -1}),
-    do: get_indices(tail, target, idx + 1, {-1, -1})
+  defp get_indices([_head | tail], target, idx, {-1, -1}) do
+    get_indices(tail, target, idx + 1, {-1, -1})
+  end
 
   defp get_indices(_arr, _target, _idx, res), do: res
 end
